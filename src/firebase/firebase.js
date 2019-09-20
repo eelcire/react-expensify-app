50c3d5dbdc1bc782ec34534e5f17b557ca099153
eelcire <eelcire.eelcire@hotmail.com>
2019-09-19 21:33:35 -0500

    9.1

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,37 +1,141 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDd8_jEZMjzp2qqu8IXil2rts_rv4hT-wQ",
-    authDomain: "expensify-21ac1.firebaseapp.com",
-    databaseURL: "https://expensify-21ac1.firebaseio.com",
-    projectId: "expensify-21ac1",
-    storageBucket: "expensify-21ac1.appspot.com",
-    messagingSenderId: "1009860408087",
-    appId: "1:1009860408087:web:a98972f6d2b7d99f7efd52"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-database.ref().set({
-    name: 'Eric Lee',
-    age: 24,
-    isSingle: true,
-    hasFriends: false,
-    location: {
-        city: 'Philadelphia',
-        country: 'United States'
-    }
-})
+export { firebase, database as default }
 
-// database.ref().set('This is my data.')
-database.ref('age').set(27)
-database.ref('location/city').set('Baton Rouge')
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val())
+// })
 
-database.ref('attributes').set({
-    height: 175,
-    weight: 162
-})
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val())
+// })
 
-console.log('I made a request to change the data')
+// database.ref('expenses').on('child_added', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val())
+// })
+
+// database.ref('expenses').on('value', (snapshot) => {
+//     const expenses = [];
+//     snapshot.forEach((thing) => {
+//         expenses.push({
+//             id: thing.key,
+//             ...thing.val()
+//         })
+//     })
+//     console.log(expenses)
+// })
+
+// database.ref('expenses')
+//     .once('value')
+//     .then((snapshot) => {
+//         const expenses = [];
+
+//         snapshot.forEach((childSnapshot) => {
+//             expenses.push({
+//                 id: childSnapshot.key,
+//                 ...childSnapshot.val()
+//         })
+//     })
+//     console.log(expenses)
+// })
+
+// database.ref('expenses').push({
+//     description: 1,
+//     note: 2,
+//     amount: 3,
+//     createdAt: 4
+// })
+
+// const notes = [{
+//     id: '12',
+//     title: 'First note!',
+//     body: 'This is my note'
+// }, {
+//     id: '761ase',
+//     title: 'Another note',
+//     body: 'This is my note'
+// }]
+
+// database.ref('NNotes').set(notes);
+
+// database.ref().on('value', (snapshot) => {
+//     const val = snapshot.val();
+//     console.log(`${val.name} is ${val.attributes.height} cm tall and weighs ${val.attributes.weight} pounds`)
+// })
+
+// const onValueChange = database.ref().on('value', (snapshot) => {
+//     console.log(snapshot.val())
+// }, (e) => {
+//     console.log('Error with data fetching', e)
+// })
+
+// setTimeout(() => {
+//     database.ref('age').set(29)
+// }, 3500)
+
+// setTimeout(() => {
+//     database.ref().off('value', onValueChange);
+// }, 7000)
+
+// setTimeout(() => {
+//     database.ref('age').set(30)
+// }, 10500)
+
+// database.ref()
+//     .once('value')
+//     .then((snapshot) => {
+//         const val = snapshot.val()
+//         console.log
+//     })
+//     .catch((e) => {
+//         console.log('Error fetching data', e)
+//     })
+
+// database.ref().set({
+//     name: 'Eric Lee',
+//     age: 24,
+//     stressLevel: 6,
+//     job: {
+//         title: 'Software developer',
+//         company: 'IBM'
+//     },
+//     location: {
+//         city: 'Philadelphia',
+//         country: 'United States'
+//     }
+// }).then(() => {
+//     console.log('Data is saved');
+// }).catch((e) => {
+//     console.log('This failed.', e);
+// })
+
+// // database.ref('isSingle')
+// //     .remove()
+// //     .then(() => {
+// //         console.log('Data was removed')
+// //     })
+// //     .catch(() => {
+// //         console.log('Did not remove data', e)
+// //     })
+
+// database.ref().update({
+//     stressLevel: 9,
+//     'job/company': 'Amazon',
+//     'location/city': 'Seattle'
+// })
